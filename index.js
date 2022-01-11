3bfd778d628651a4c1c827689a4fc7bd1a83eb48
Kimberly Hayes <kshayes78@gmail.com>
2022-01-11 08:35:17 -0500

    commit tree b9a5a44d9af87543385baa5278cc07fc0c304e76

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 31
+let num2 = 2
+const multiply = num1 * num2
 
-// Write your code here
+let random = Math.floor(Math.random()*10)+1;
+console.log(random);
+
+let num3 = 5
+let num4 = 29
+const mod = num4 % num3
+
+let max = Math.max(5, 10, 15, 20)
